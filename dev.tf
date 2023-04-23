a37061a03e632639699e1fbaef56a663570bdde6
Randhir <randhir2021@gmail.com>
2023-04-23 19:51:11 +0530

    adding files

--- dev.tf
+++ dev.tf
@@ -1,5 +1,5 @@
 module "ec2-dev" {
-  source         = "../modules/ec2-instance"
+  source         = "./modules/ec2-instance"
   instance_type  = "t2.micro"
   instance_count = 1
   environment    = "dev"
